c91de396c24fbf6e1b61b7711023c1838f6025cd
Adam Gajzlerowicz <nelf86@gmail.com>
2018-03-27 16:17:55 +0200

    push

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,20 +4,28 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
 
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    |> mirror_row
+  end
+
+
+  def mirror_row(data) do
+    data
   end
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
           |> :binary.bin_to_list
-
     %Identicon.Image{hex: hex}
   end
 
   def pick_color(image = %Identicon.Image{hex: [r, g, b | _tale ]}) do
-
     %Identicon.Image{image | color: {r, g, b }}
-
   end
 
 
