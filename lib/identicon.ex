8aa92cb564876081f88e921201564b809a7f21f7
Adam Gajzlerowicz <nelf86@gmail.com>
2018-03-29 13:44:10 +0200

    ready to draw

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,17 +5,41 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
   end
 
-  def build_grid(%Identicon.Image{hex: hex} = _image) do
-    hex
-    |> Enum.chunk(3)
-    |> mirror_row
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn ({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+      {top_left, bottom_right}
+    end
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    odd = Enum.filter grid, fn ({code, _idx}) ->
+      rem(code, 2) == 0
+    end
+    %Identicon.Image{image | grid: odd}
+
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid = hex
+           |> Enum.chunk(3)
+           |> Enum.map(&mirror_row/1)
+           |> List.flatten
+           |> Enum.with_index
+    %Identicon.Image{image | grid: grid}
   end
 
 
-  def mirror_row(data) do
-    Enum.map(data, fn([a, b, c])-> [a, b, c, b, a] end )
+  def mirror_row([a, b, c]) do
+    [a, b, c, b, a]
   end
 
 
@@ -26,8 +50,8 @@ defmodule Identicon do
   end
 
 
-  def pick_color(image = %Identicon.Image{hex: [r, g, b | _tale ]}) do
-    %Identicon.Image{image | color: {r, g, b }}
+  def pick_color(image = %Identicon.Image{hex: [r, g, b | _tale]}) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
 
